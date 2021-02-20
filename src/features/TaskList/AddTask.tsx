da9226ba4812fa7341f92390f3ee660ae1a89af7
BonIlcer <pantaev.e@mail.ru>
2021-02-21 04:11:29 +0700

    Update styles

--- src/features/TaskList/AddTask.tsx
+++ src/features/TaskList/AddTask.tsx
@@ -1,6 +1,6 @@
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
 import React from 'react'
-import { Button, TextField } from '@material-ui/core'
+import { Box, Button, ClickAwayListener, TextField } from '@material-ui/core'
 import { Add } from '@material-ui/icons'
 import { maxIdState, tasksState } from '../../state'
 
@@ -29,13 +29,15 @@ export default function AddTask({ projectId }: { projectId: number }) {
   }
   if (!adding) {
     return (
-      <div
+      <Box
         style={{
           color: '#aaa',
           padding: '8px',
           paddingLeft: '0px',
           paddingTop: '8px',
+          cursor: 'pointer',
         }}
+        onClick={() => setAdding(true)}
       >
         <span>
           <Add
@@ -47,70 +49,62 @@ export default function AddTask({ projectId }: { projectId: number }) {
             }}
           />
         </span>
-        <span
-          style={{ fontSize: '16px', fontFamily: 'Exo 2 rev=1' }}
-          onClick={() => setAdding(true)}
-        >
-          Add task
-        </span>
-      </div>
+        <span style={{ fontSize: '16px', fontFamily: 'Exo 2 rev=1' }}>Add task</span>
+      </Box>
     )
   } else {
     return (
-      <div
-        style={{
-          color: '#aaa',
-          padding: '8px',
-          paddingLeft: '0px',
-          paddingTop: '0px',
-          flexGrow: 1,
-        }}
-      >
-        <div>
-          <div style={{ padding: '6px', paddingLeft: 0, paddingRight: 0 }}>
-            <TextField
-              variant='outlined'
-              size='small'
-              autoFocus
-              style={{ width: '100%' }}
-              inputProps={{ style: { padding: '6px' } }}
-              value={taskName}
-              onChange={(e) => setTaskName(e.target.value)}
-              onKeyUp={(e) => {
-                if (e.key === 'Escape') {
-                  setAdding(false)
-                  setTaskName('')
-                } else if (e.key === 'Enter') {
-                  addTask()
-                }
-                e.preventDefault()
-                e.stopPropagation()
-              }}
-            />
-          </div>
-          <div style={{ padding: '0px' }}>
-            <Button
-              variant='contained'
-              size='small'
-              disabled={taskName.length === 0}
-              onClick={addTask}
-            >
-              Add Task
-            </Button>
-            <Button
-              onClick={() => setAdding(false)}
-              size='small'
-              style={{
-                color: '#aaa',
-                marginLeft: '6px',
-                fontFamily: 'Exo 2 rev=1',
-              }}
-            >
-              Cancel
-            </Button>
+      <ClickAwayListener onClickAway={() => setAdding(false)}>
+        <div
+          style={{
+            color: '#aaa',
+            padding: '8px',
+            paddingLeft: '0px',
+            paddingTop: '0px',
+            flexGrow: 1,
+          }}
+        >
+          <div>
+            <div style={{ padding: '6px', paddingLeft: 0, paddingRight: 0 }}>
+              <TextField
+                variant='outlined'
+                size='small'
+                autoFocus
+                style={{ width: '100%' }}
+                inputProps={{ style: { padding: '6px' } }}
+                value={taskName}
+                onChange={(e) => setTaskName(e.target.value)}
+                onKeyUp={(e) => {
+                  if (e.key === 'Escape') {
+                    setAdding(false)
+                    setTaskName('')
+                  } else if (e.key === 'Enter') {
+                    addTask()
+                  }
+                  e.preventDefault()
+                  e.stopPropagation()
+                }}
+              />
+            </div>
+            <div style={{ padding: '0px' }}>
+              <Button variant='contained' size='small' disabled={taskName.length === 0} onClick={addTask}>
+                Add Task
+              </Button>
+              <Button
+                onClick={() => setAdding(false)}
+                size='small'
+                style={{
+                  color: '#aaa',
+                  marginLeft: '6px',
+                  fontFamily: 'Exo 2 rev=1',
+                }}
+              >
+                Cancel
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </ClickAwayListener>
     )
   }
 }
--- src/features/TaskList/Task.tsx
+++ src/features/TaskList/Task.tsx
@@ -2,26 +2,11 @@ import { useUpdateAtom } from 'jotai/utils'
 import { assoc, filter } from 'ramda'
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
-import {
-  Checkbox,
-  IconButton,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  TextField,
-  Typography,
-} from '@material-ui/core'
+import { Checkbox, IconButton, ListItem, ListItemIcon, ListItemText, TextField, Typography } from '@material-ui/core'
 import { Delete, DragIndicator } from '@material-ui/icons'
 import { Task, Todo, taskState, tasksState } from '../../state'
 
-export default function TaskItem({
-  item,
-  index,
-}: {
-  projectId: number
-  item: Todo
-  index: number
-}) {
+export default function TaskItem({ item, index }: { projectId: number; item: Todo; index: number }) {
   const setTasks = useUpdateAtom(tasksState)
   const setTask = useUpdateAtom(taskState(item.id))
   const [hover, setHover] = React.useState(false)
@@ -40,10 +25,7 @@ export default function TaskItem({
         <ListItem
           ref={provided.innerRef}
           {...provided.draggableProps}
-          style={getItemStyle(
-            snapshot.isDragging,
-            provided.draggableProps.style
-          )}
+          style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
@@ -56,11 +38,7 @@ export default function TaskItem({
           >
             <DragIndicator style={{ color: '#666', fontSize: 20 }} />
           </ListItemIcon>
-          <Checkbox
-            checked={item.status}
-            size='small'
-            onChange={toggleCompletion}
-          />
+          <Checkbox checked={item.status} size='small' onChange={toggleCompletion} />
           <ListItemText
             disableTypography={true}
             onClick={() => {
@@ -106,8 +84,8 @@ export default function TaskItem({
             style={{
               color: '#666',
               visibility: hover ? 'visible' : 'hidden',
-              padding: 0,
-              paddingRight: '10px',
+              padding: 6,
+              marginRight: 0,
             }}
             onClick={onDelete}
           >
