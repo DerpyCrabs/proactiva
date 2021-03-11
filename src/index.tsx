16bee854bda6baad3465b77c83b2563e67de021d
BonIlcer <pantaev.e@mail.ru>
2021-03-11 19:38:15 +0700

    Update mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import { Provider } from 'jotai'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'jotai'
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { blue } from '@material-ui/core/colors'
 import { createMuiTheme } from '@material-ui/core/styles'
+import './index.css'
 import Index from './pages/Index'
 
 const theme = createMuiTheme({
@@ -11,6 +12,9 @@ const theme = createMuiTheme({
     type: 'dark',
     primary: {
       main: blue[500],
+    },
+    background: {
+      default: '#222222',
     },
   },
   typography: {
