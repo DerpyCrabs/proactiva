a4bc31ae9d5947f12b7cfce3beb22b8893d78fc8
BonIlcer <pantaev.e@mail.ru>
2021-07-03 04:19:55 +0700

    Remove dialog breakpoints color styling

--- packages/web/src/components/Dialog.tsx
+++ packages/web/src/components/Dialog.tsx
@@ -14,32 +14,20 @@ import { useIsMobile } from '../utils'
 
 const styles = (theme: Theme) =>
   createStyles({
-    paper: {
-      //padding: theme.spacing(1),
+    paper: {      
       width: '100%',
 
-      [theme.breakpoints.up('xs')]: {
-        color: '#f00',
-      },
       [theme.breakpoints.up('sm')]: {
-        color: '#0f0',
         margin: theme.spacing(2),
         maxWidth: `calc(100% - ${theme.spacing(2) * 2}px)`,
       },
-      [theme.breakpoints.up('md')]: {
-        color: '#00f',
+      [theme.breakpoints.up('md')]: {        
         margin: theme.spacing(4),
-        //maxWidth: `calc(100% - ${theme.spacing(8) * 2}px)`,
         maxWidth: '860px',
-      },
-      [theme.breakpoints.up('lg')]: {
-        color: '#ff0',
-      },
-      [theme.breakpoints.up('xl')]: {
-        color: '#0ff',
-      },
+      },      
     },
   })
+
 const Dialog = ({
   scroll = 'body',
   maxWidth = false,
