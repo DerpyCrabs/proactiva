61c01b55bad3c0391ffa6031458803b359b8b5dd
BonIlcer <pantaev.e@mail.ru>
2021-07-03 08:08:05 +0700

    Update Dialog title style

--- packages/web/src/components/Dialog.tsx
+++ packages/web/src/components/Dialog.tsx
@@ -7,48 +7,51 @@ import {
   Dialog as MuiDialog,
   Theme,
   Typography,
+  createStyles,
+  withStyles,
 } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
-import { createStyles, withStyles } from '@material-ui/styles'
 import { useIsMobile } from '../utils'
 
 const styles = (theme: Theme) =>
   createStyles({
-    paper: {      
+    paper: {
       width: '100%',
 
       [theme.breakpoints.up('sm')]: {
         margin: theme.spacing(2),
         maxWidth: `calc(100% - ${theme.spacing(2) * 2}px)`,
       },
-      [theme.breakpoints.up('md')]: {        
+      [theme.breakpoints.up('md')]: {
         margin: theme.spacing(4),
         maxWidth: '860px',
-      },      
+      },
+    },
+
+    dialogTitle: {
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
     },
   })
 
 const Dialog = ({
   scroll = 'body',
   maxWidth = false,
-  onClose,
-  children,
   title,
-  ...others
-}: DialogProps & { title: String; onClose: () => void }) => {
-
+  classes,
+  children,
+  onClose,
+  ...restProps
+}: DialogProps & { title: string; onClose: () => void; classes: any }) => {
   const fullScreen = useIsMobile()
   return (
-    <MuiDialog
-      fullScreen={fullScreen}
-      scroll={scroll}
-      maxWidth={maxWidth}
-      onClose={onClose}
-      {...others}
-    >
+    <MuiDialog fullScreen={fullScreen} scroll={scroll} maxWidth={maxWidth} onClose={onClose} {...restProps}>
       <DialogTitle>
         <Box display='flex' justifyContent='space-between' alignItems='center'>
-          <Typography variant='h6'> {title}</Typography>
+          <Typography variant='h6' className={classes.dialogTitle}>
+            {title}
+          </Typography>
           <IconButton aria-label='close' onClick={onClose} size='small'>
             <CloseIcon fontSize='small' />
           </IconButton>
